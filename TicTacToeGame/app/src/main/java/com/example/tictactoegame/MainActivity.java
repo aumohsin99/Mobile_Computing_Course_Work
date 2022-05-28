3570d13d0452415e60e47783af1bd24f0195b38e
Moshin <bsef19m031@pucit.edu.pk>
2022-05-28 12:12:22 +0500

    Almost done with game

--- TicTacToeGame/app/src/main/java/com/example/tictactoegame/MainActivity.java
+++ TicTacToeGame/app/src/main/java/com/example/tictactoegame/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.tictactoegame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.TextureView;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    boolean gameActive=true;
 
     // player representation
     // 0 - o
@@ -28,6 +31,11 @@ public class MainActivity extends AppCompatActivity {
     {
         ImageView img = (ImageView) view;
         int tappedImage = Integer.parseInt(img.getTag().toString());
+        if (!gameActive)
+        {
+            gameReset(view);
+        }
+
         if (gamestate[tappedImage]==2)
         {
             gamestate[tappedImage]=activeplayer;
@@ -36,19 +44,59 @@ public class MainActivity extends AppCompatActivity {
             {
                 img.setImageResource(R.drawable.o);
                 activeplayer=1;
+                TextView status = findViewById(R.id.status);
+                status.setText("X's Turn Now");
             }
             else
             {
                 img.setImageResource(R.drawable.x);
                 activeplayer=0;
+                TextView status = findViewById(R.id.status);
+                status.setText("O's Turn Now");
             }
 
             img.animate().translationYBy(1000f).setDuration(300);
         }
 
+        // checking if someone won
+        for (int[] winpostion: winPositions) {
+            if (gamestate[winpostion[0]] == gamestate[winpostion[1]] &&
+                    gamestate[winpostion[1]] == gamestate[winpostion[2]] && gamestate[winpostion[0]] != 2) {
+                String winnerStr;
+                gameActive = false;
+                if (gamestate[winpostion[0]] == 0) {
+                    winnerStr = "Player 0 won!";
+                } else {
+                    winnerStr = "Player X won!";
+                }
 
+                TextView status = findViewById(R.id.status);
+                status.setText(winnerStr);
+
+            }
+        }
+    }
+    
+    public void gameReset(View view)
+    {
+        gameActive=true;
+        activeplayer=0;
+        for (int i=0; i< gamestate.length; i++)
+        {
+            gamestate[i]=2;
+        }
+        ((ImageView)findViewById(R.id.imageView0)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView1)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView2)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView3)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView4)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView5)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView6)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView7)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView8)).setImageResource(0);
 
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
