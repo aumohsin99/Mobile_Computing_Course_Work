919bd67b607c4e0440e15bab6f15a788475a9d29
Moshin <bsef19m031@pucit.edu.pk>
2022-05-30 22:20:03 +0500

    first commit

---
+++ List_View_Practice/app/src/main/java/com/example/list_view_practice/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.list_view_practice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView myListView = findViewById(R.id.myListView);
+        ArrayList<String> grocery = new ArrayList<>();
+        grocery.add("Bhindi");
+        grocery.add("Pen");
+        grocery.add("Bag");
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<>(context: this, Android.R.Layout
+
+
+
+
+
+    }
+}
