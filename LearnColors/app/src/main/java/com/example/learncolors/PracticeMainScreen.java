9487ddfb18fcf931bd48384d41314d28569eee29
Moshin <bsef19m031@pucit.edu.pk>
2022-06-06 23:31:35 +0500

    main repo update

--- LearnColors/app/src/main/java/com/example/learncolors/PracticeMainScreen.java
+++ LearnColors/app/src/main/java/com/example/learncolors/PracticeMainScreen.java
@@ -1,39 +1,47 @@
 package com.example.learncolors;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import java.lang.Math;
+import java.util.Map;
 
-public class PracticeMainScreen extends AppCompatActivity implements View.OnClickListener {
+public class PracticeMainScreen extends AppCompatActivity implements View.OnClickListener
+{
 
     public int randomgenerator(int min, int max)
     {
-        int index = (int)(Math.random()*(max-min+1)+min);
+        int index = (int) (Math.random() * (max - min + 1) + min);
         return index;
     }
 
-   // int index = (int)(Math.random()*(max-min+1)+min);
+    // int index = (int)(Math.random()*(max-min+1)+min);
 //   int min = 1;
 //    int max= 12;
-    int index= randomgenerator(1, 12);
-    String[] colorsarray = {"Red", "Green", "Yellow", "Black", "Blue", "Brown", "Gray", "Orange", "Pink", "Purple", "White" };
+    int index = randomgenerator(1, 12);
+
+    String[] colorsarray = {"Red", "Green", "Yellow", "Black", "Blue", "Brown", "Gray", "Orange", "Pink", "Purple", "White"};
+
+
+    //Map<String, String> dictionaryone = new Map<String, String>() ;
 
 
     Button opt1button, opt2button, opt3button, opt4button;
 
-   // int index = 0;
+    // int index = 0;
 
-   //  int index = 5;
-   // String colorsarray[];
+    //  int index = 5;
+    // String colorsarray[];
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice_main_screen);
 
@@ -48,27 +56,63 @@ public class PracticeMainScreen extends AppCompatActivity implements View.OnClic
         opt3button.setOnClickListener(this);
         opt4button.setOnClickListener(this);
 
-        
-
 
         TextView questionheadingview = findViewById(R.id.questionheading);
-        questionheadingview.setText("Select button in "+ colorsarray[index] + " color:");
-
+        questionheadingview.setText("Select button in " + colorsarray[index] + " color:");
 
 
     }
 
 
+    // String[] colorsarray = {"Red", "Green", "Yellow", "Black", "Blue", "Brown", "Gray", "Orange", "Pink", "Purple", "White" };
 
-   // String[] colorsarray = {"Red", "Green", "Yellow", "Black", "Blue", "Brown", "Gray", "Orange", "Pink", "Purple", "White" };
-
-
-
-    String hell = "bye";
+   // opt1button =
+//    opt1button.setBackgroundColor(Android.Graphics.Color.parseColor("#738b28"));
+//
+//    opt1button.set
+//  opt1button.setBakc(Color.parseColor("#97a0ad"));
+//
+//
+//            if opt1button.
+//    String hell = "bye";
 
 
     @Override
-    public void onClick(View view) {
+    public void onClick(View view)
+    {
+        Intent intent = new Intent(PracticeMainScreen.this, PracticeMainScreen.class);
+        switch (view.getId())
+        {
+            case R.id.opt1button:
+                intent.putExtra("text", "A");
+                // intent.putExtra("picture", R.drawable.a);
+                startActivity(intent);
+                break;
+
+            case R.id.opt2button:
+                intent.putExtra("text", "B");
+                // intent.putExtra("picture", R.drawable.b);
+                startActivity(intent);
+                break;
+
+            case R.id.opt3button:
+                intent.putExtra("text", "C");
+                // intent.putExtra("picture", R.drawable.c);
+                startActivity(intent);
+
+                break;
+
+            case R.id.opt4button:
+                intent.putExtra("text", "D");
+                //intent.putExtra("picture", R.drawable.d);
+                startActivity(intent);
+                break;
+        }
+    }
+
+    public void quiztry1(String[] colorsarray[index], )
+    {
 
     }
+
 }
